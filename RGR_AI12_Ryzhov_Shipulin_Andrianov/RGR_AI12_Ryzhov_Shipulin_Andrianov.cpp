730f5e017527107f05eee9a94fcc436e160f3694
dfg dfg <artur.uns.dk@mail.ru>
2022-05-31 22:03:41 +0700

    Заполнение таблицы вижинера для англ

--- RGR_AI12_Ryzhov_Shipulin_Andrianov/RGR_AI12_Ryzhov_Shipulin_Andrianov.cpp
+++ RGR_AI12_Ryzhov_Shipulin_Andrianov/RGR_AI12_Ryzhov_Shipulin_Andrianov.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int Vijiner();
 
+map<int, vector<char>> TableEng;
 
-
-int main()
-{
+int main(){
+	
 
 
 	setlocale(LC_ALL, "Rus");
@@ -20,17 +20,29 @@ int main()
 int Vijiner() {
     vector<char>Case = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};// и так понятно что это
     /*std:vector<char>Symbol = { ' ', '+','!','@','#','$','%','^','&','*','[',']',')','(','=','`',':',';','{','}','.',',','<','>','"','?','/','\'' }*///; // это тоже понятно
-	map<int, vector<char>> Table;
+	map<int, vector<char>> TableEng;
 	for (int KeyTable = 0; KeyTable < Case.size();KeyTable++) {
 			char SymbolMove;
-			Table[KeyTable] = Case;
+			TableEng[KeyTable] = Case;
 			SymbolMove = Case[0];
+			cout << KeyTable;
+			for (int k = 0; k < Case.size();k++) {
+				cout << Case[k];
+			}
+			cout << '\n';
 			Case.erase(Case.begin());
 			Case.push_back(SymbolMove);
 
-		
-
 	}
+	cout << '\t' << '\t' << "Vijiner's Table for English" << '\n' << '\n';
+
+
+	
+	
 	return 0;
 }
+void encrypt(string key) {
 
+
+
+}
