93c94339fe7cbe1e872b69454c46b3b41a805c28
dfg dfg <artur.uns.dk@mail.ru>
2022-06-01 11:36:41 +0700

    Шифровка и расшифровка англ. слов вернего регистра. Слова считываются с пробелом

--- RGR_AI12_Ryzhov_Shipulin_Andrianov/RGR_AI12_Ryzhov_Shipulin_Andrianov.cpp
+++ RGR_AI12_Ryzhov_Shipulin_Andrianov/RGR_AI12_Ryzhov_Shipulin_Andrianov.cpp
@@ -2,17 +2,32 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <string>
 using namespace std;
 
 int Vijiner();
+void decrypt(string key,string word);
 void encrypt(string key, string wordfoenc);
 map<int, vector<char>> TableEng;
-vector<char>Case = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+vector<char>Case = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','_'};
+
 
 int main(){
+	string word, key;
+	cout << "Input word: ";
+	getline(cin, word);
 
+	cout << "Input key: ";
+	(cin >> key).get();
+	for (int i = 0;i < word.size(); i++) {
+		if (word[i] ==' ') {
+			word[i] = '_';
+		}
+	}
+	cout << "\t" << word << "\n";
 	Vijiner();
-	encrypt("LOX", "GAYLORD");
+	encrypt(key, word);
+	decrypt(key, word);
 	setlocale(LC_ALL, "Rus");
 
 	
@@ -67,3 +82,32 @@ void encrypt(string key,string wordfoenc) {
 	}
 	cout << '\t' << EncWord << '\n';
 }
+
+void decrypt(string key, string wordfodec) {
+	if (key.size() < wordfodec.size()) {
+		for (int i = key.size(), k = 0;i < wordfodec.size();i++, k++) {
+			key = key + key[k];
+		}
+	}
+	string DecWord;
+	int IndexKey = 0;
+	int IndexSymb = 0;
+	for (int i = 0; i < key.size();i++) {
+		for (int n = 0;n < Case.size();n++) {
+			if (Case[n] == key[i]) {
+				IndexKey = n;
+				break;
+			}
+		}
+		for (int n = 0;n < Case.size();n++) {
+			if (Case[n] == wordfodec[i]) {
+				IndexSymb = n;
+				break;
+			}
+		}
+		vector<char> encr = TableEng[0];
+		DecWord += encr[IndexSymb];
+	}
+	cout << '\t' << DecWord << '\n';
+}
+
