be60dd388d4258e924886ae59dccc0bf4f3fed6e
dfg dfg <artur.uns.dk@mail.ru>
2022-06-05 20:32:55 +0700

    Подготовка для свомещения

--- RGR_AI12_Ryzhov_Shipulin_Andrianov/main.cpp
+++ RGR_AI12_Ryzhov_Shipulin_Andrianov/main.cpp
@@ -1,5 +1,6 @@
 #include "vijglobal.h"
 #include "vijfunc.h"
+#include <conio.h>
 
 vector<char>CaseEng = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 vector<char>CaseEngLow = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
@@ -10,23 +11,38 @@ int main() {
 	setlocale(LC_ALL, "ru");
 	int exit = 1;
 	while (exit == 1) {
-		cout << "Введите текст для шифровки и дешифровки на английском языке: " << endl;
-		string checklist;
-		getline(cin, checklist);
-		/*block for caesar
-		*/
-		/*
-		block for polobiya
-		*/
-		/*block for vijiner*/
-		cout << "\t" << "Шифр Вижинера" << "\n";
-		cout << "Введите ключ для шифровки и деширофки алгоритмом Вижинера: ";
-		string key;
-		cin >> key;
-		key = keycheck(key, checklist);
-		Vijiner();
-		string EncWord = encrypt(key, checklist);
-		decrypt(key, EncWord);
+		int choose;
+		cout << "Шифр Цезаря :: 1" << endl;
+		cout << "Шифр Полибия :: 2" << endl;
+		cout << "Шифр Вижинера :: 3" << endl;
+		cout << "Выберите алгортим шифрования: ";
+		cin >> choose;
+
+		switch (choose)
+		{
+		case 1: {
+
+		}
+		case 2: {
+
+		}
+		case 3: {		
+			cout << "\t" << "Шифр Вижинера" << "\n";
+			cout << "Введите текст для шифровки и дешифровки на английском языке: " << endl;
+			string text;
+			getline(cin, text);
+			cout << "Введите ключ для шифровки и деширофки алгоритмом Вижинера: " << "\n";
+			string key;
+			cin >> key;
+			key = keycheck(key, text);
+			Vijiner();
+			string EncWord = encrypt(key, text);
+			decrypt(key, EncWord);
+		}
+		default:
+			cout << "BREAK";
+			break;
+		}
 
 	}
 }
