3002e362d65e8bf21fdf90dc17bfcc87e7dacb7e
Дмитрий Сергеевич Шипулин <chief.dima24@yandex.ru>
2022-06-06 16:11:56 +0300

    .

---
+++ RGR_AI12_Ryzhov_Shipulin_Andrianov/PolibDoc.cpp
@@ -0,0 +1,47 @@
+#include "bibliotec.h"
+
+
+int mainy() {
+    setlocale(LC_ALL, "russian");
+    system("chcp 1251");
+    system("cls");
+    {   char* string = new char[36];
+    const int m = 6;
+    const int n = 6;
+    int i, j;
+    char A[m][n] = { { 'А', 'Б', 'В', 'Г', 'Д', 'Е'},
+                    { 'Ж', 'З', 'И', 'К', 'Л', 'М'},
+                    { 'Н', 'О', 'П', 'Р', 'С', 'Т'},
+                    { 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш'},
+                    { 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю'},
+                    { 'Я', '.', ',', '?', ';', ':' } };
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << setw(2) << A[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    cout << "Введите зашифрованное слово : ";
+    cin >> string;
+
+    for (int k = 0; k < strlen(string); k++)
+    {
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (string[k] == A[i][j])
+                {
+                    cout << A[i][j] << setw(2);
+                }
+            }
+        }
+    }
+    cout << endl;  }
+    system("pause");
+    return 0;
+}
